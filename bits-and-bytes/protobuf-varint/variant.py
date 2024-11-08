2cd6a556e2ac7b56b5e9ef6a6a998a1eee488e84
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2024-11-08 11:57:22 -0700

    init

---
+++ bits-and-bytes/protobuf-varint/variant.py
@@ -0,0 +1,17 @@
+"""
+-read the files with hexdump?
+-interpret as int
+
+implement variant encoder
+
+implement decoder
+
+
+"""
+
+if __name__ == "__main__":
+  import os
+  one = open(r"1.uint64")
+  one_fifty = open(r"150.uint64")
+  max = open(r"maxint.uint64")
+  print(one)
