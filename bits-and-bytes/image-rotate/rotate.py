ca4ca004df0aba2be7fc3f62a26cab9cd5f80bcc
Alex Gilvarry <alex.gilvarry@gmail.com>
2024-11-16 16:57:29 -0700

    broken teapot

--- bits-and-bytes/image-rotate/rotate.py
+++ bits-and-bytes/image-rotate/rotate.py
@@ -3,24 +3,30 @@ import os
 if os.path.exists('rotated.bmp'):
     os.remove('rotated.bmp')
 
-with open('teapot.bmp', 'rb') as f, open('rotated.bmp', 'wb') as s:
-
+with open('teapot.bmp', 'rb') as file, open('rotated.bmp', 'wb') as s:
+    f = file.read()
+    start_address= int.from_bytes(f[10:11], "little")
+    height = int.from_bytes(f[18:22], "little")
+    width = int.from_bytes(f[22:26], "little")
+    out=[]
+    img = f[start_address:]
+    img_len = len(img)
+    s.write(f[:start_address])
+    quarter_len = img_len//4
+    quarter_one = f[start_address: quarter_len]
+    rest = f[quarter_len:]
     
-    res = f.read()
-    start_address= int.from_bytes(res[10:11], "little")
-    height = int.from_bytes(res[18:22], "little")
-    width = int.from_bytes(res[22:26], "little")
-    out=0
-    out <<= start_address
+    out = bytearray()
     
-    img = res[start_address:]
-    out = res[0:start_address]
-    out += res[0:start_address]
-    print(start_address)
-    print(res[18:22], res[22:26])
-    print(type(out))
-    n=bytearray(res)
-    for i, b in enumerate(n):
-        if i>= start_address:
-            n[i] = 0
-    s.write(bytes(n))
+    # out <<= quarter_len
+    # out.append(rest)
+    # out.append(quarter_one)
+    # b = bytes(out) 
+    s.write(bytes(b for b in rest))
+    s.write(bytes(b for b in quarter_one))
+    # im
+    # print(img_len)
+    # for i, b in enumerate(n):
+    #     if i>= start_address:
+    #         n[i] = 0
+    # s.write(bytes(n))
