d33a28543f7f7c442882448a23b049217f1fea15
Alex Gilvarry <alex.gilvarry@gmail.com>
2024-11-18 14:11:36 -0700

    rotate accomplished

--- bits-and-bytes/image-rotate/rotate.py
+++ bits-and-bytes/image-rotate/rotate.py
@@ -3,30 +3,24 @@ import os
 if os.path.exists('rotated.bmp'):
     os.remove('rotated.bmp')
 
-with open('teapot.bmp', 'rb') as file, open('rotated.bmp', 'wb') as s:
+with open('stretch-goal.bmp', 'rb') as file, open('stretch-rotated.bmp', 'wb') as s:
     f = file.read()
+    
     start_address= int.from_bytes(f[10:11], "little")
-    height = int.from_bytes(f[18:22], "little")
+    # height = int.from_bytes(f[18:22], "little")
     width = int.from_bytes(f[22:26], "little")
-    out=[]
-    img = f[start_address:]
-    img_len = len(img)
     s.write(f[:start_address])
-    quarter_len = img_len//4
-    quarter_one = f[start_address: quarter_len]
-    rest = f[quarter_len:]
+
+    img = f[start_address:]
     
-    out = bytearray()
+    new_rows = [ [] for _ in range(width)]
+    pixels = [img[i: i+3] for i in range(0, len(img), 3)]
+    rows = [pixels[i: i+width] for i in range(0, len(pixels), width)]
     
-    # out <<= quarter_len
-    # out.append(rest)
-    # out.append(quarter_one)
-    # b = bytes(out) 
-    s.write(bytes(b for b in rest))
-    s.write(bytes(b for b in quarter_one))
-    # im
-    # print(img_len)
-    # for i, b in enumerate(n):
-    #     if i>= start_address:
-    #         n[i] = 0
-    # s.write(bytes(n))
+    for row in rows:
+        for j, pixel in enumerate(row):
+            new_rows[j].append(pixel)
+
+    for row in reversed(new_rows):
+       for pixel in row:
+            s.write(bytes(pixel))
