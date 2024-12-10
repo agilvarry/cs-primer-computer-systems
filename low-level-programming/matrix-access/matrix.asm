2468c271c28d8696745ea6ed2ecbcf9a6972267f
Alex Gilvarry <alex.gilvarry@gmail.com>
2024-12-10 10:52:27 -0700

    matix update

--- low-level-programming/matrix-access/matrix.asm
+++ low-level-programming/matrix-access/matrix.asm
@@ -1,14 +1,19 @@
 section .text
 global index
 index:
+	; imul ecx, edx
+	; add ecx, r8d
+	; imul ecx, 4
+	; add rcx, rdi
+	; mov eax, [rcx]
+
 	; rdi: matrix
 	; esi: rows
 	; edx: cols
 	; ecx: rindex
 	; r8d: cindex
+
 	imul ecx, edx
 	add ecx, r8d
-	; imul ecx, 4
-	; add rcx, rdi
-	mov rax, [rdi + 4 *rcx]
-    ret
+	mov rax, [rdi + 4 * rcx]
+	ret
